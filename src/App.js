1a9b76601064a861353af4d4c83b386da7ec7989
Glen Maddern <glenmaddern@gmail.com>
2017-10-12 11:57:49 +1100

    object splat and rerendering triggers

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const AppHeader = (props) => (
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo"/>
-    <h1 className="App-title">
-      Welcome to React
-      { props.clicked ? '!!!!' : '' }
-    </h1>
-  </header>
-)
-
-const obj = {
-  id: '1234',
-  foo() {
-    console.log("Foo got called with `this`:")
-    console.log(this)
-  }
+const AppHeader = (props) => {
+  console.log("Rendering AppHeader")
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <h1 className="App-title">
+        Welcome to React
+        {props.clicked ? '!!!!' : ''}
+      </h1>
+    </header>
+  )
 }
-//obj.foo = obj.foo.bind(obj)
 
-const fn = obj.foo
-fn()
+const obj = { a: 1, b: 2, c: 3, d: 5 }
+console.log(obj)
+
+const { foo, ...otherKeys } = obj
+console.log(foo, otherKeys)
+
+const AppIntro = () => {
+  console.log("Rendering AppIntro")
+  return (
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  )
+}
 
 class App extends React.Component {
   state = { clicked: false }
@@ -32,12 +38,11 @@ class App extends React.Component {
   }
 
   render() {
+    console.log("Rendering App")
     return (
       <div className="App">
         <AppHeader clicked={this.state.clicked}/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppIntro/>
         <p>
           <button type="button" onClick={this.handleClick}>
             Click me!
