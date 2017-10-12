6e593139e3977a4a564c913fb7c33396cf33bf72
Glen Maddern <glenmaddern@gmail.com>
2017-10-12 11:42:29 +1100

    showing click callbacks and the weirdness of JS this

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-class App extends Component {
+const AppHeader = (props) => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo"/>
+    <h1 className="App-title">
+      Welcome to React
+      { props.clicked ? '!!!!' : '' }
+    </h1>
+  </header>
+)
+
+const obj = {
+  id: '1234',
+  foo() {
+    console.log("Foo got called with `this`:")
+    console.log(this)
+  }
+}
+//obj.foo = obj.foo.bind(obj)
+
+const fn = obj.foo
+fn()
+
+class App extends React.Component {
+  state = { clicked: false }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <AppHeader clicked={this.state.clicked}/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <button type="button" onClick={this.handleClick}>
+            Click me!
+          </button>
         </p>
       </div>
     )
